00e19f7fb3d5c465772f103c4d2c980a38b33b30
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-21 01:49:43 +0430

    - // Add BookGetCollectionRequest file Add rules

---
+++ app/Http/Requests/BookGetCollectionRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
+
+class BookGetCollectionRequest extends FormRequest
+{
+    
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+       
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        
+        return [
+            'page' => 'integer|min:1',
+            'sortColumn' => 'in:title,avg_review',
+            'sortDirection' => 'in:ASC,DESC',
+            'title'=> 'string',
+            'authors'=> 'string',
+
+
+        ];
+    }
+}
