f5ef5cceb00ad584d46e89c1f695f1ca5813ad78
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-21 01:50:06 +0430

    - // Add functionality to toArray

--- app/Http/Resources/AuthorResource.php
+++ app/Http/Resources/AuthorResource.php
@@ -19,7 +19,9 @@ class AuthorResource extends JsonResource
     public function toArray($request)
     {
         return [
-            //@todo code here
+            "id" => $this->id,
+            "name" => $this->name,
+            "surname" =>$this->surname,
         ];
     }
 }
