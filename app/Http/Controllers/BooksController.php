d7c4cc93d7fc44919c21b67049b5ef43f23a2eb7
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-21 01:47:58 +0430

    - // Add BookGetCollectionRequest Replace Request with BookGetCollectionRequest Complete getCollection function Add getAllBooksPaginate function

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -13,12 +13,71 @@ use App\Http\Resources\BookReviewResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-
+use App\Http\Requests\BookGetCollectionRequest;
 class BooksController extends Controller
 {
-    public function getCollection(Request $request)
+    public function getCollection(BookGetCollectionRequest $request)
     {
-        //@todo code here
+        $request->validated();
+       
+        
+        $books = $this->getAllBooksPaginate($request);
+        return BookResource::collection($books);
+    }
+    
+    protected function getAllBooksPaginate($request)
+    {
+        $title = $request->get('title',false);
+        $authorsId = false;
+        $sortColumn = $request->get('sortColumn', false);
+        $sortDirection = $request->get('sortDirection', false);
+
+        $isDescSorting =  $sortDirection == 'DESC' ? true : false;
+        $isAvgReview = $sortColumn == 'avg_review';
+
+        if ($request->has('authors')) {
+            if(str_contains($request->authors , ',')){
+                $authorsId = explode(',', $request->authors);
+            }else{
+                $authorsId = [$request->authors];  
+            }
+        }
+     
+        
+        $baseQuery = Book::when($authorsId , function($query, $authorsId){
+            $query->whereHas('authors', function($query) use ($authorsId) {
+                $query->whereIntegerInRaw('id', $authorsId);
+            });
+
+            },
+            function($query){
+                $query->with('authors');
+            })
+        ->when($title, function ($query, $title) {
+             $query->where('title',$title)->orWhere('title', 'like', '%' . $title . '%');
+        })
+        ->with('reviews');
+     
+        
+        $paginator = $baseQuery->paginate();
+        if ($sortColumn ){
+            $paginator = $paginator->getCollection()->sortBy(
+                function ($book, $key) use($isAvgReview) {
+                    if($isAvgReview){
+                        return $book->reviews->avg('review'); 
+                    }                
+                    return $book['title'];
+                },
+                SORT_REGULAR,
+                $isDescSorting);
+
+        }
+        
+        
+ 
+        return $paginator;
+
+        
     }
 
     public function post(PostBookRequest $request)
