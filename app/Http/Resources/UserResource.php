83c25fe31ba8c3047ca846ed5a939c6066abad7b
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-21 04:59:09 +0430

    - // Add attributes to `toArray`

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,8 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            //@todo code here
+            "id" => $this->id,
+            "name" => $this->name
         ];
     }
 }
