503a3b576e9c9f5850a36a6f8245f6d54eabc933
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-21 04:56:56 +0430

    - // Add rules

--- app/Http/Requests/PostBookRequest.php
+++ app/Http/Requests/PostBookRequest.php
@@ -12,9 +12,18 @@ class PostBookRequest extends FormRequest
      * @return array
      */
     public function rules(): array
-    {
+    {   
+        /**
+         * Issue with digits,
+         * when isbn set to digits:13 and user pass array to it then return error 500 with wired exception
+         * fast fix is to use range of 1000000000000 (first 13 digits number) and 9999999999999 (last 13 digits number)
+         */
         return [
-            //@todo code here
+            'isbn'=>'required|numeric|unique:books|max:9999999999999|min:1000000000000',
+            'title'=>'required|string',
+            'description'=>'required|string',
+            'authors'=>'required|array',
+            'authors.*'=>'required|exists:authors,id|distinct'   
         ];
     }
 }
